9b133114fecb45163721a5f09b537760416feb85
Victor Wang <37973874+vyzw@users.noreply.github.com>
2019-01-26 22:54:31 -0800

    text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import { parse, definedWords } from './Parser/parse';
 
-const testString = "{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}";
+let testString = "{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}{de|A de is a what|y'+2=0|y''+2=0|How do we solve a de ?}{ode|A ode is a what|y'+2=0|}";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      str: '',
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        {parse(testString, definedWords(testString))}
+        <textarea id="txtarea">{testString}</textarea>
+        <button onClick={() => this.setState({str: document.getElementById("txtarea").value})}>Compile</button>
+        {parse(this.state.str, definedWords(this.state.str))}
       </div>
     );
   }
