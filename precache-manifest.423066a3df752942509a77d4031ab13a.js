37d92f0faf935bfe0c73b1b103a2648a77572b79
Victor Wang <37973874+vyzw@users.noreply.github.com>
2019-01-27 10:17:49 -0800

    Updates

---
+++ precache-manifest.423066a3df752942509a77d4031ab13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eb2a0be1bdb7f4d8c7423da5256fe4c",
+    "url": "/context/static/media/widelogo.0eb2a0be.png"
+  },
+  {
+    "revision": "35c551177e3fe07a1311",
+    "url": "/context/static/js/runtime~main.35c55117.js"
+  },
+  {
+    "revision": "cdfd53a422884d96425a",
+    "url": "/context/static/js/main.cdfd53a4.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/context/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "cdfd53a422884d96425a",
+    "url": "/context/static/css/main.43b66edc.chunk.css"
+  },
+  {
+    "revision": "b650e4a55e5e3b9e2a95fe416781ec79",
+    "url": "/context/index.html"
+  }
+];
